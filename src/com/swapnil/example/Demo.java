03388ed630c61a20ccac36981ea1a339b2ff3d9d
Swapnil Mailare <swapnilmailare501@gmail.com>
2024-05-22 08:06:38 +0530

    My first commit

---
+++ src/com/swapnil/example/Demo.java
@@ -0,0 +1,21 @@
+package com.swapnil.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+
+	}
+	public void m1()
+	{
+		List<String> list= new ArrayList<String>();
+		
+		list.add("Swapnil");
+		list.add("Vaibhav");
+		
+	}
+
+}
